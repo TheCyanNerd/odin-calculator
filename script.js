7c3f6e332d9a3c128435842bce5e0d77af6cecfe
Salman Rashid <32942339+TheCyanNerd@users.noreply.github.com>
2023-07-02 00:38:31 -0400

    Arithmetic fn: subtraction

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function add(a, b) {
   b = (typeof b == 'number') ? b : parseInt(b);
   return a + b;
 }
+
+// subtract(a, b) takes numbers (or number Strings) and returns their difference
+function subtract(a, b) {
+  a = (typeof a == 'number') ? a : parseInt(a);
+  b = (typeof b == 'number') ? b : parseInt(b);
+  return a - b;
+}
